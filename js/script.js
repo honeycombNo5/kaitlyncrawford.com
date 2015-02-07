56dac484534d2e11c0c29314f70d7ad999dc1805
Kaitlyn Crawford <kcrawford29@gmail.com>
2015-02-06 21:08:38 -0600

    full page masonry working almost

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('#content').masonry({
+   columnWidth: 320,
+   itemSelector: '.item'
+  })
+});
+
+// or with jQuery
+// initialize Masonry
+var $container = $('#container').masonry();
+// layout Masonry again after all images have loaded
+$container.imagesLoaded( function() {
+  $container.masonry();
+});
